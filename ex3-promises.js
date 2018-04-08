1e02ed60cd04f3f7ec89b4d9a6debd4ff319a1ca
donkeykong91 <darylnator@gmail.com>
2018-04-08 06:07:55 -0700

    finished promise version

--- ex3-promises.js
+++ ex3-promises.js
@@ -21,7 +21,43 @@ function output(text) {
 
 function getFile(file) {
 	// what do we do here?
+	let text;
+	fakeAjax(file, function() {
+		text = file;
+	});
+	// return a promise
+	return new Promise(
+		function(reslove, reject) {
+			if (text) resolve(text);
+			else error();
+	});
 }
 
+function finish() {
+	output(arguments[0]);
+	output("Complete!");
+}
+
+function error() {
+	output("Something when wrong!");
+}
+
+var getFile1 = getFile("file1");
+var getFile2 = getFile("file2");
+var getFile3 = getFile("file3");
+
+getFile1()
+.then(function(text) {
+	output(text);
+	return getFile2();
+})
+.then(function(text) {
+	output(text);
+	return getFile3();
+})
+.then(
+	finish,
+	error
+);
 // request all files at once in "parallel"
 // ???

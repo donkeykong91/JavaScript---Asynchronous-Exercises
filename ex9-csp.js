8f1a29c29f98e852fbe9d1b03833c5b292a8e2d8
donkeykong91 <darylnator@gmail.com>
2018-04-13 03:00:47 -0700

    Added fix

--- ex9-csp.js
+++ ex9-csp.js
@@ -1,17 +1,47 @@
 $(document).ready(function(){
 	var $btn = $("#btn"),
-		$list = $("#list");
+		$list = $("#list"),
+		clicks = ASQ.csp.chan(),
+		msgs = ASQ.csp.chan(),
+		queuedClick;
 
 	$btn.click(
-		ASQ.csp.putAsync(ch, evt);
+		ASQ.csp.putAsync(listenToClicks);
 	);
 
+	// run go routines
 	ASQ().runner(
-		ASQ.csp.go(function*() {
-				yield ASQ.csp.take(ch);
-				yield ASQ.csp.timeout(500);
-				$list.append($("<div>" + msg + "</div>"));
-			}
-		);
+				ASQ.csp.go(sampleClicks);
+				ASQ.csp.go(logClick);
 	);
+
+	// push the click event messages into channel
+	function listenToClicks(evt) {
+		if (!queuedClick){
+			queuedClick = ASQ.csp.putAsync(clicks, evt);
+			queuedClick.then(function() {
+				queuedClick = null;
+			});
+		}
+	}
+
+	// sample clicks channel
+	function *sampleClicks() {
+		while(true) {
+			yield ASQ.csp.take(
+				ASQ.csp.timeout(1000)
+			);
+			yield ASQ.csp.take(clicks);
+			yield ASQ.csp.put(msgs, "Clicked!");
+		}
+	}
+
+	// subscribe to sampled message channel
+	function *logClick() {
+		while(true) {
+			var msg = yield ASQ.csp.take(msgs);
+			$list.append($("<div>" + msg + "</div>"));
+		}
+	}
+
 });
